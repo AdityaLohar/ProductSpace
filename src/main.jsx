6d5d598775eb1e5f282aa5a7d34d954304d4b3e3
Aditya Lohar <77808500+AdityaLohar@users.noreply.github.com>
2024-09-23 10:53:05 +0530

    removed vercel tracking

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { Analytics } from '@vercel/analytics/react'
+// import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <Analytics />
+      {/* <Analytics /> */}
       <App />
     </Router>
   </StrictMode>,
