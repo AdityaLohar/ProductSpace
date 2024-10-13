180dc0fbf8d23d0843c912380af5ba7986f00794
Aditya Lohar <77808500+AdityaLohar@users.noreply.github.com>
2024-10-13 19:24:15 +0530

    changed prices

--- src/components/PmFellowshipHeroSection.jsx
+++ src/components/PmFellowshipHeroSection.jsx
@@ -108,13 +108,13 @@ const PmFellowshipHeroSection = () => {
               backgroundRepeat: "no-repeat",
             }}
           >
-            <span className="font-bold">20%</span> <br /> OFF
+            <span className="font-bold">15%</span> <br /> OFF
           </div>
           <div className="text-start pt-2">
             <p className="text-[14px]">👉🏻 Best price for next 5 seats</p>
             <div className="flex gap-2 items-end">
               <p className="text-[24px] md:text-[32x] lg:text-[40px] font-bold text-black font-sans">
-                ₹23,999
+                ₹25,499
               </p>
               <p className="text-[18px] line-through pb-1">₹29,999</p>
             </div>
@@ -130,7 +130,7 @@ const PmFellowshipHeroSection = () => {
                 />
               </div>
               <div>
-                <p>Offer valid till 13th Oct 2024</p>
+                <p>Offer valid till 17th Oct 2024</p>
               </div>
             </div>
 
