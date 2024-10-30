964ad399b6ee8942a591239aae7ee7c68d4ec677
Aditya Lohar <77808500+AdityaLohar@users.noreply.github.com>
2024-10-30 17:40:39 +0530

    newslteer updated

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import netLeft from "../assets/net-pattern-left.svg";
+import netRight from "../assets/net-pattern-right.svg";
 import { z } from "zod";
-import { FaCheckCircle, FaExclamationCircle } from "react-icons/fa"; // Import success/error icons
+import { FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
 
 const schema = z.object({
   email: z.string().email("Invalid email address"),
@@ -25,7 +27,8 @@ const NewsLetter = () => {
       setNotification({
         type: "success",
         title: "Subscription Successful",
-        description: "Thank you for subscribing to our newsletter! Get ready for the latest insights and updates delivered right to your inbox. 🚀",
+        description:
+          "Thank you for subscribing to our newsletter! Get ready for the latest insights and updates delivered right to your inbox. 🚀",
       });
       setShowNotification(true);
     }
@@ -37,33 +40,73 @@ const NewsLetter = () => {
   };
 
   return (
-    <div className="flex flex-col bg-[#F5F5F5] pb-20 pt-16">
-      <div className="py-6 lg:pb-8">
-        <div className="text-[24px] lg:text-[40px] font-bold text-center font-sans px-4">
+    <div className="bg-transparent px-4 md:px-10 xl:px-32 pt-10 lg:pt-10 pb-16 font-inter">
+      <div
+        className="hidden lg:flex bg-black text-white flex-col py-12 rounded-2xl max-w-[1180px] mx-auto items-between justify-between text-center gap-6"
+        style={{
+          backgroundImage: `url(${netLeft}), url(${netRight})`,
+          backgroundPosition: "left 10px top, right 10px top", // Adjust as needed
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain", // Adjust to fit your images as needed
+        }}
+      >
+        <div className="text-[24px] lg:text-[40px] font-bold px-2 font-sans">
           Subscribe to our Newsletter
         </div>
-        <div className="text-[14px] lg:text-[16px] text-center text-gray-700 font-medium px-2 md:px-20 lg:px-60 py-2">
+
+        <div className="text-[18px] text-[#FFFFFFE5] font-light">
           <p>
             Subscribe to our Newsletter to stay up-to-date on the latest
             courses, news, and discounts from us.
           </p>
         </div>
+
+        <div className="flex justify-center items-center mt-0 lg:mt-10 px-2">
+          <div className="flex justify-between items-center bg-white rounded-lg p-1 lg:p-1 mx-2 lg:mx-0 w-full lg:w-3/5">
+            <input
+              type="text"
+              placeholder="Enter your Email Id"
+              className="px-3 rounded-full text-gray-800 input font-semibold w-1/2 lg:w-2/3 placeholder-black bg-transparent focus:outline-none"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button
+              className="bg-black text-white py-3 px-3 lg:px-5 rounded-lg font-medium text-[16px] lg:text-base"
+              onClick={handleSubmit}
+            >
+              Subscribe
+            </button>
+          </div>
+        </div>
       </div>
 
-      <div className="flex justify-center items-center mt-0 lg:mt-10 px-4">
-        <div className="flex justify-between items-center bg-white border border-2 border-[#c3c3c3] rounded-full p-1 lg:p-2 mx-2 lg:mx-0 w-full lg:w-3/5">
-          <input
-            type="text"
-            placeholder="Enter your Email Id"
-            className="px-3 rounded-full text-gray-800 input font-semibold w-1/2 lg:w-2/3 placeholder-gray-500 bg-transparent focus:outline-none"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button
-            className="bg-[#F5F5F5] text-[#111] py-2 px-3 lg:px-3 rounded-full font-semibold text-[16px] lg:text-base"
-            onClick={handleSubmit}
-          >
-            Subscribe Now
-          </button>
+      <div className="lg:hidden bg-black text-white flex flex-col py-24 px-4 rounded-2xl max-w-[1180px] mx-auto items-between justify-between text-center gap-8">
+        <div className="text-[24px] lg:text-[40px] font-bold px-2 font-sans">
+          Subscribe to Our Newsletter
+        </div>
+
+        <div className="text-[16px] text-[#FFFFFFE5] font-light px-4">
+          <p>This is your chance to secure your spot in the next cohort!</p>
+          <p>
+            Have doubts? Book a 1:1 call with Product Space&apos;s leading
+            mentors.
+          </p>
+        </div>
+
+        <div className="flex justify-center items-center mt-0 px-2">
+          <div className="flex justify-between items-center bg-white rounded-lg p-1 mx-2 w-full">
+            <input
+              type="text"
+              placeholder="Enter your Email Id"
+              className="px-2 rounded-full text-black input font-semibold w-full placeholder-black bg-transparent focus:outline-none"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button
+              className="bg-black text-white py-3 px-4 rounded-lg font-light text-[16px]"
+              onClick={handleSubmit}
+            >
+              Subscribe
+            </button>
+          </div>
         </div>
       </div>
 
@@ -71,9 +114,7 @@ const NewsLetter = () => {
       {notification && (
         <div
           className={`fixed left-1/2 transform -translate-x-1/2 ${
-            showNotification
-              ? "bottom-8 opacity-100"
-              : "-bottom-20 opacity-0"
+            showNotification ? "bottom-8 opacity-100" : "-bottom-20 opacity-0"
           } transition-all duration-500 ease-in-out z-50 max-w-[340px] md:max-w-[400px] w-full`}
         >
           <div
