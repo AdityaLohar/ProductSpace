c1f1173f25d0a680d54a25504c628ae90ca915fc
Aditya Lohar <77808500+AdityaLohar@users.noreply.github.com>
2024-11-15 13:15:29 +0530

    alums images fixed

--- src/components/ReviewPmFellowship.jsx
+++ src/components/ReviewPmFellowship.jsx
@@ -6,6 +6,15 @@ import { useState } from "react";
 import linkedinLogo from "../assets/linkedin-circle.svg";
 import fullStar from "../assets/star.svg";
 import halfStar from "../assets/star-half.svg";
+import adityaPant from "../assets/aditya-pant.png";
+import medhya from "../assets/medhya-budhwar.png";
+import aryan from "../assets/aryan-jaiswal.png";
+import mrigaj from "../assets/mrigaj.jpeg";
+import jaydeep from "../assets/jaydeep.jpeg";
+import adityaMandothia from "../assets/aditya-mandothia.jpeg";
+import prashant from "../assets/prashant-kumar.jpeg";
+import nishant from "../assets/nishant-sinha.jpeg";
+import anshul from "../assets/anshul-agarwal.jpeg";
 
 
 const StarRating = ({ rating }) => {
@@ -132,7 +141,7 @@ const reviews = [
     title: "You HAVE to do this. The insights are 🔥",
     desc: "Thank you, Product Space and mentor Akhil Yash Tiwari, for the incredible support and upskilling. From day one, the hands-on approach and focus on fundamentals helped me apply my knowledge to real-life scenarios. The dedication and encouragement from Akhil were truly inspiring, pushing us to continuously improve!",
     username: "Aditya Pant",
-    img: "https://media.licdn.com/dms/image/v2/D4D03AQGYmAzuGoncIg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1718714447992?e=1731542400&v=beta&t=7INAN9eGo7UEPiQj3HOmCAfrD9CySxe8113oD-AZL3Y",
+    img: adityaPant,
     company: "Samaro",
     post: "Product Management Intern",
     linkedin: "https://www.linkedin.com/in/adityapant--",
@@ -142,7 +151,7 @@ const reviews = [
     title: "Great Quality",
     desc: "Grateful to Product Space for the in-depth knowledge and skills in product management. The practical experience and 1:1 support were exceptional",
     username: "Aditya Mandothia",
-    img: "https://media.licdn.com/dms/image/v2/D5603AQGGLNTiBlHcrw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1710504658899?e=1731542400&v=beta&t=Bc1TFPzuuo0uEN4fuf-1tdXKDwYl3OF2yW31-PwAQIo",
+    img: adityaMandothia,
     company: "Cinestox",
     post: "Product Management Intern",
     linkedin: "https://www.linkedin.com/in/aditya-mandothia-84882520a/",
@@ -152,7 +161,7 @@ const reviews = [
     title: "Amazing Experience",
     desc: "The mentors at Product Space went above and beyond to help me achieve my goals. Their guidance was crucial to my success.",
     username: "Nishant Sinja",
-    img: "https://media.licdn.com/dms/image/v2/D5603AQFuaisZpsSTjw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1705161505684?e=1731542400&v=beta&t=aThtTNNdBqxkVgYOfBP394wH95SeA8T1ZANwJZ1s_18",
+    img: nishant,
     company: "CreditAccess Grameen Limited",
     post: "Product Manager",
     linkedin: "https://www.linkedin.com/in/nishant-sinha-134701134",
@@ -162,7 +171,7 @@ const reviews = [
     title: "Good Support",
     desc: "The mentorship at Product Space has been key to my growth as a Product Manager. The sessions, feedback, and mock interviews greatly enhanced my skills and confidence. I'm truly grateful for the unwavering support and belief in my potential. Special thanks to Akhil Yash Tiwari for his consistent guidance throughout our journey. Here's to ongoing growth and success! I highly recommend Product Space for anyone looking to refine their skills as a Product Manager.",
     username: "Mrigaj Nirvan Goradia",
-    img: "https://media.licdn.com/dms/image/v2/D4D03AQGk2o03hDHZJg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1700811766381?e=1731542400&v=beta&t=Tzw0dFdaveqGVCDC0J59IkxdCtpAntGombGD0so5dxA",
+    img: mrigaj,
     company: "Kaam",
     post: "Product Manager",
     linkedin: "https://www.linkedin.com/in/mrigaj-goradia-b93408107",
@@ -172,7 +181,7 @@ const reviews = [
     title: "Highly Recommend",
     desc: "Grateful to Product Space and their mentors for upskilling me in Product Management and interview strategies. Highly recommend it for anyone looking to elevate their skills!",
     username: "Anshul Agarwal",
-    img: "https://media.licdn.com/dms/image/v2/D4D03AQFb1RR6l5Nufg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1694013700834?e=1731542400&v=beta&t=X4wG_7rX198PAeNMDMT8Gjrgb-GvWMf8NkzosVLVbZw",
+    img: anshul,
     company: "My Sivi",
     post: "Product Manager",
     linkedin: "https://www.linkedin.com/in/a4sh",
@@ -182,7 +191,7 @@ const reviews = [
     title: "Very Satisfied",
     desc: "I'm incredibly grateful to Product Space for their 1:1 mentorship. The personalized guidance and support I received truly transformed my career journey. The mentors at Product Space provided me with the skills and confidence I needed to succeed. I couldn’t have achieved this transition without their invaluable assistance!",
     username: "Aryan Jaiswal",
-    img: "https://media.licdn.com/dms/image/v2/C4E03AQG_j9Ls8iygIA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1626771255085?e=1731542400&v=beta&t=jMNNAPrrttnDHspafglJ2JHJMA1g0RG4B2qRK9mp5Oc",
+    img: aryan,
     company: "Ind Money",
     post: "Product Management Trainee",
     linkedin: "https://www.linkedin.com/in/aryanjaiswal2401/",
@@ -192,7 +201,7 @@ const reviews = [
     title: "Transformative Experience",
     desc: "Product Space made learning truly worthwhile, starting from basics and building up to Tech for PM with hands-on tools and peer engagement. The live projects were a great experience, and the 1-on-1 mentorship was invaluable. Special thanks to Akhil Yash Tiwari for being the go-to person for all my doubts!",
     username: "Jaydeep Prajapati",
-    img: "https://media.licdn.com/dms/image/v2/D5603AQFDFdylPDq9Cw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1719207899519?e=1731542400&v=beta&t=lDyr4Or3LiSEBLtIuI9ptYt6PvOZ7tMViqLs4gkTLIU",
+    img:  jaydeep,
     company: "WiJungle",
     post: "Product Manager Internship",
     linkedin: "https://www.linkedin.com/in/jaydeep-prajapati-26ab3a210/",
@@ -202,7 +211,7 @@ const reviews = [
     title: "Amazing ",
     desc: "I would like to express my heartfelt gratitude to Akhil Yash Tiwari and the entire Product Space team. The exceptional learning resources and guidance I received were truly transformative. Your mentorship played a crucial role in upskilling me and helping me grow both personally and professionally. Thank you for the unwavering support and dedication.",
     username: "Medhya Budhwar",
-    img: "https://media.licdn.com/dms/image/v2/D4D03AQH_pXGG_9D_kw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1693849888611?e=1731542400&v=beta&t=B710fpF3oSVoLYtGPBG3C6NeEgwneupn60Sa75vphMo",
+    img: medhya,
     company: "Swiggy",
     post: "Product Management Consultant",
     linkedin: "https://www.linkedin.com/in/medhya-budhwar-27715420a",
@@ -212,7 +221,7 @@ const reviews = [
     title: "Fantastic Journey",
     desc: "Reflecting on my journey, it feels like just yesterday I started with uncertainty and doubt. I'm deeply grateful to my mentor, Akhil Yash Tiwari, and Product Space for their support and guidance, which were key to my growth and success.",
     username: "Prashant Kumar",
-    img: "https://media.licdn.com/dms/image/v2/D4D03AQEFgVh9-d586A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1693849733017?e=1731542400&v=beta&t=KebkYAScNXHaDiKUEW0-EOQ8P7P9XmeXK9Pxz2wCRvA",
+    img: prashant,
     company: "Bento Labs",
     post: "Product Management Intern",
     linkedin: "https://www.linkedin.com/in/prashant-kumar-5440b421b/",
