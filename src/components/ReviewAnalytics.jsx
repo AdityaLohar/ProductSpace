dc2c9ca1e08ab7f1ed0387953a64013186ccb404
AdityaLohar <adityalohar55@gmail.com>
2024-12-13 18:18:45 +0530

    reviws design updated

--- src/components/ReviewAnalytics.jsx
+++ src/components/ReviewAnalytics.jsx
@@ -58,21 +58,35 @@ const ReviewCardSmall = ({
   const [expand, setExpand] = useState(desc.length <= 340);
 
   return (
-    <div className="text-[16px] w-[300px]">
-      {" "}
-      {/* Set width to 100vw */}
-      <div className="flex flex-col relative gap-2 bg-white border border-2 border-transparent hover:border-[#C3C3C3] shadow-lg rounded-lg p-4 py-6">
+    <div
+      className="
+        text-[16px] 
+        w-[250px] 
+        h-auto 
+        md:w-[400px] 
+        md:h-[300px]
+        overflow-hidden
+        shadow-[0_0_20px_rgba(0,0,0,0.1)]
+        rounded-lg
+      "
+    >
+      <div
+        className="flex flex-col relative gap-2 bg-white border border-2 border-transparent 
+        hover:border-[#C3C3C3] rounded-lg p-4 py-6 h-full"
+      >
         <div>
-          <p>{expand ? desc : desc.slice(0, 340)}</p>
-          <button
-            onClick={() => setExpand(!expand)}
-            className="text-blue-600 bg-none border-none cursor-pointer py-4"
-          >
-            {desc.length > 340 && (expand ? "Read less" : "Read more")}
-          </button>
+          <p>{expand ? desc : `${desc.slice(0, 340)}...`}</p>
+          {desc.length > 340 && (
+            <button
+              onClick={() => setExpand(!expand)}
+              className="text-blue-600 bg-none border-none cursor-pointer py-4"
+            >
+              {expand ? "Read less" : "Read more"}
+            </button>
+          )}
         </div>
 
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mt-auto">
           <div className="text-yellow-500">
             <StarRating rating={rating} />
           </div>
@@ -116,18 +130,13 @@ const reviews = [
 
 const ReviewAnalytics = () => {
   return (
-    <div
-      className="flex flex-col bg-[#F5F5F5] pb-5 pt-3 lg:pt-0"
-      style={{
-        background: "radial-gradient(circle, #00C1FD3A 30%, white 70%)",
-      }}
-    >
+    <div className="flex flex-col pt-3 lg:pt-0 rounded-lg">
       <div className="py-5 lg:py-12">
         <div className="text-[20px] lg:text-[28px] font-semibold">
           Reviews from previous attendees
         </div>
 
-        <div className="pm-reviews-scrollbar flex overflow-x-scroll gap-6 pt-8 pl-4 md:pl-8">
+        <div className="pm-reviews-scrollbar flex overflow-x-scroll gap-6 pt-8 pl-4 md:pl-8 pb-4">
           {reviews.map((review, index) => (
             <div className="" key={index}>
               <ReviewCardSmall
