e8a419481f6835745a58b543796db2c718b442e7
AdityaLohar <adityalohar55@gmail.com>
2024-12-30 18:48:10 +0530

    pm offer date changed

--- src/components/PmFellowshipHeroSection.jsx
+++ src/components/PmFellowshipHeroSection.jsx
@@ -137,7 +137,7 @@ const PmFellowshipHeroSection = () => {
                 />
               </div>
               <div>
-                <p>Offer valid till 30th Dec 2024</p>
+                <p>Offer valid till 31st Dec 2024</p>
               </div>
             </div>
 
