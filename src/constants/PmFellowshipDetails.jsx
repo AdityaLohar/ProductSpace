d998fa4880a50252f4fefb3da32b169ff6e34963
AdityaLohar <adityalohar55@gmail.com>
2025-01-06 23:02:46 +0530

    pm fellowship details changed

--- src/constants/PmFellowshipDetails.jsx
+++ src/constants/PmFellowshipDetails.jsx
@@ -1,7 +1,7 @@
 const PM_FELLOWSHIP_DETAILS = {
   price: 29999,
-  discount: 15,
-  valid: "6th Jan 2025",
+  discount: 10,
+  valid: "10th Jan 2025",
   paymentLink: "https://rzp.io/l/getintoPM",
   startDate: "2025-01-12",
   startDateText: "Jan 12, 2025",
