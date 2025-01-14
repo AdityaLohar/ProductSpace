dfc2f63e48772b2ecb2947d400c8f4b054ec1226
Surya Vikram <suryavikram00>
2025-01-14 16:09:06 +0530

    ai pm date preponed to first of feb

--- src/constants/GenAiDetails.jsx
+++ src/constants/GenAiDetails.jsx
@@ -2,8 +2,8 @@ const GEN_AI_FOR_PM_DETAILS = {
   price: 24999,
   discount: 40,
   valid: "17th Jan 2025",
-  startDate: "2025-02-08",
-  startDateText: "Feb 08, 2025",
+  startDate: "2025-02-01",
+  startDateText: "Feb 01, 2025",
   brochureDriveLink: "https://drive.google.com/file/d/19gpZ_azSzEIcGgbPHeBUt-XNGyfvZYSA/view?usp=sharing",
 };
 
