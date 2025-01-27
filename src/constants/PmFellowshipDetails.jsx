30c88405c9688808c2259d188fdc6d48673ffc3d
Surya Vikram <suryavikram00>
2025-01-27 10:05:18 +0530

    changed the offer valid till 31st jan 2025

--- src/constants/PmFellowshipDetails.jsx
+++ src/constants/PmFellowshipDetails.jsx
@@ -1,7 +1,7 @@
 const PM_FELLOWSHIP_DETAILS = {
   price: 34999,
   discount: 35,
-  valid: "20th Jan 2025",
+  valid: "31st Jan 2025",
   paymentLink: "https://rzp.io/l/getintoPM",
   startDate: "2025-02-22",
   startDateText: "Feb 22, 2025",
